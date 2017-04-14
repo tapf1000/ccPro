3fb40891233695009af15ebdd66f11b071a357b9
tapfuma1000 <tapfuma12march@gmail.com>
2017-04-14 11:49:28 +0200

    Create page2.php

---
+++ page2.php
@@ -0,0 +1,20 @@
+ <!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p>
+ <?php
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+?> </p>
+
+<p>This is a paragraph.</p>
+
+</body>
+</html> 
